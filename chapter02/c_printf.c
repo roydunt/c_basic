fa15324c5bb9bcbb0a120fbf7b74c2ae712858cc
roydunt <112936385+roydunt@users.noreply.github.com>
2022-09-19 10:48:39 +0900

    finished chapter01

---
+++ chapter02/c_printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// 제어 문자
+// + \n : 한줄 개행
+// + \t : 탭 (8칸 스페이스)
+int main(void)
+{
+   /* code */
+    printf("Hello\n");
+    printf("Chulwoong \t Choi");
+
+    return 0;
+}
