0fb7ae55e0509b159d79c071594a4bbdf9c3ad93
roydunt <112936385+roydunt@users.noreply.github.com>
2022-11-14 10:41:34 +0900

    Add files via upload

---
+++ chapter07/c_method_recursive.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * 재귀함수(recursive function) (중요)
+ *  - 자기자신을 호출하는 함수
+ *  - return(반환값) > 다시 자기자신을 호출하는 함수를 사용
+*/
+
+void fruit(void);
+
+int main(void) {
+    fruit();
+}
+
+//무한 반복 호출하는 재귀함수
+//  - 절대 사용 금지
+//  - Fatal Error;
+
+void fruit(void) {
+    printf("apple\n");
+    fruit();
+}
---
+++ chapter07/c_method_return.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void print_char(char ch, int count);
+
+int main(void) {
+    print_char("*", 5);
+}
+
+void print_char(char ch, int count) {
+    for(int i=0; i<count; i++) {
+        printf("%c", ch);
+    }
+    // 함수를 호출하고 종료되면 다시 호출했던 곳으로 돌아감
+    // 1.함수 종료 > return 문을 만나면 종료
+    // 2.함수 종료 > }를 만나면 종료
+    return;
+}
